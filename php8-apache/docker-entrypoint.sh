65ca0b84bb7760eb1ae0951617b7451911a970af
mosowell <mark.proxorofff@gmail.com>
2021-11-27 17:29:05 +0300

    Теперь при монтировании /var/www/html возможно читать и записывать файлы /var/www/html, как со стороны докер-хоста, так и со стороны контейнера одновременно.
    Теперь владелец директории /var/www/html это www-data:www-data

---
+++ php8-apache/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Данный docker-entrypoint.sh предоставляет возможность читать и записывать файлы в VOLUME /var/www/html/
+# со стороны докер-хоста и со стороны контейнера одновременно,
+# путем создания пользователя в контейнере с таким же UID как и у пользователя докер-хоста
+# Подробнее https://denibertovic.com/posts/handling-permissions-with-docker-volumes/
+
+# По умолчанию USER_ID равен 1000
+USER_ID=${DOCKER_HOST_USER_ID:-1000}
+echo "Starting with UID : $USER_ID"
+usermod -u 1000 www-data
+chown -R www-data:www-data /var/www/html/
+
+exec "$@"
